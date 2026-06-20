30836661ae95a8e9d0f38fc4376e116b8b4beb5c
HHO2026 <abagailk@heritageharborottawa.com>
2026-06-19 19:33:02 -0500

    Enhance map.js with user location features

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView(
-[41.3456, -88.8426],
+[41.3456,-88.8426],
 13
 );
 
@@ -9,18 +9,99 @@ L.tileLayer(
 {
 attribution:'OpenStreetMap'
 }
-)
-.addTo(map);
+
+).addTo(map);
 
 
 
 L.marker(
 [41.3456,-88.8426]
 )
+
 .addTo(map)
 
 .bindPopup(
-"Heritage Harbor E-Bike Pickup"
+"🚲 Heritage Harbor E-Bike Pickup"
+);
+
+
+
+// FIND LOCATION BUTTON
+
+var userMarker;
+
+
+
+document.getElementById("locateBtn")
+.onclick = function(){
+
+
+map.locate({
+
+setView:true,
+
+watch:true,
+
+enableHighAccuracy:true
+
+});
+
+
+};
+
+
+
+
+// WHEN LOCATION FOUND
+
+map.on(
+'locationfound',
+
+function(e){
+
+
+if(userMarker){
+
+userMarker.setLatLng(e.latlng);
+
+}
+
+else{
+
+
+userMarker = L.marker(
+e.latlng
 )
 
-.openPopup();
+.addTo(map)
+
+.bindPopup(
+"📍 You are here"
+);
+
+
+}
+
+
+
+}
+);
+
+
+
+
+// IF LOCATION FAILS
+
+map.on(
+
+'locationerror',
+
+function(){
+
+alert(
+"Please allow location access to use this feature."
+);
+
+}
+
+);
