d11028f3212f9d01b3f932402d1b9ad61f37d0eb
HHO2026 <abagailk@heritageharborottawa.com>
2026-06-19 19:16:29 -0500

    Fix formatting and clean up map.js

--- map.js
+++ map.js
@@ -1,11 +1,14 @@
 var map = L.map('map').setView(
-[41.3456,-88.8426],
+[41.3456, -88.8426],
 13
 );
 
 
 L.tileLayer(
-'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+{
+attribution:'OpenStreetMap'
+}
 )
 .addTo(map);
 
@@ -18,18 +21,6 @@ L.marker(
 
 .bindPopup(
 "Heritage Harbor E-Bike Pickup"
-);
-
-
-
-L.marker(
-[41.322,-88.990]
 )
 
-.addTo(map)
-
-.bindPopup(
-
-"Starved Rock Adventure"
-
-);
+.openPopup();
